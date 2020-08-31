b70c45de4893dce8acce1bd680e6614549872fde
Nikolay Voronov <nkvoronov@gmail.com>
2020-08-31 13:27:46 +0300

    fixes

--- default.py
+++ default.py
@@ -1,19 +1,19 @@
 from lib.utils import *
 
 log('script version %s started' % ADDONVERSION)
-    service = ADDON.getSettingBool('service')
+service = ADDON.getSettingBool('service')
 
 if sys.argv == [''] and not service:
-            log('service not enabled')
+    log('service not enabled')
 elif len(sys.argv) == 2 and sys.argv[1] == 'test':
     from lib.scrapertest import *
-            test_scrapers()
+    test_scrapers()
 elif not WIN.getProperty('culrc.running') == 'true':
     from lib import gui
     gui.MAIN(mode=service)
 elif not WIN.getProperty('culrc.guirunning') == 'TRUE':
     WIN.setProperty('culrc.force','TRUE') # we're already running, user clicked button on osd
-        else:
+else:
     log('script already running')
     if not ADDON.getSettingBool('silent'):
         xbmcgui.Dialog().notification(ADDONNAME, LANGUAGE(32158), time=2000, sound=False)
